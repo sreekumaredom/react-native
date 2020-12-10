f20145b2c145415bf1e67129a9567d853c8f3b56
Anandhan <anandhanedom@gmail.com>
2020-12-10 22:05:55 +0530

    styling and scroll

--- 00002_rnGuide/App.js
+++ 00002_rnGuide/App.js
@@ -1,30 +1,83 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  Button,
+} from 'react-native';
 
 export default function App() {
+  const [goalInput, setGoalInput] = useState('');
+  const [courseGoals, setcourseGoals] = useState([]);
+
+  const goalInputHandler = (text) => {
+    setGoalInput(text);
+  };
+
+  const addGoalHandler = () => {
+    setcourseGoals([
+      ...courseGoals,
+      { uid: Math.random().toString(), value: goalInput },
+    ]);
+  };
+
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
-          style={{
-            width: '80%',
-            borderColor: 'black',
-            borderWidth: 1,
-            padding: 10,
-          }}
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={goalInput}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      {/* Pre-renders everything down the list - use only for a small list 
+      <ScrollView >
+        {courseGoals.map((goal, i) => (
+          <View key={i} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+      <FlatList
+        keyExtractor={(item, index) => item.uid}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
